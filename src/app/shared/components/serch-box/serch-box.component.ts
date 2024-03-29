99b418fd9d05f8b372087578f57c95b9089e0a19
Fenx <jonalvarez143@gmail.com>
2024-03-29 21:49:44 +0100

    seccion 10 acabda

--- src/app/shared/components/serch-box/serch-box.component.ts
+++ src/app/shared/components/serch-box/serch-box.component.ts
@@ -1,18 +1,53 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-serch-box',
   templateUrl: './serch-box.component.html',
   styles: ``,
 })
-export class SerchBoxComponent {
+export class SerchBoxComponent implements OnInit, OnDestroy {
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSuscription?: Subscription;
+
   @Input()
   public placeholder: string = '';
+
+  @Input()
+  public initialValue: string = '';
 
   @Output()
   public onValue = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncerSuscription = this.debouncer
+      .pipe(debounceTime(350))
+      .subscribe((value) => {
+        console.log('debounce value', value);
+        this.onDebounce.emit(value);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSuscription?.unsubscribe();
+  }
+
   emitValue(value: string): void {
     this.onValue.emit(value);
+  }
+
+  onKeyPress(searchTerm: string) {
+    // console.log(searchTerm);
+    this.debouncer.next(searchTerm);
   }
 }
